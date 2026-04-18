19bad56fbcc46a6b86f70336b2042f06ea06a6cd
LucasTNgo <moothemoo@tamu.edu>
2026-04-18 00:29:39 -0500

    Tweaked HeaterCharacterization

--- Tools/HeaterCharacterization/HeaterCharacterization.cpp
+++ Tools/HeaterCharacterization/HeaterCharacterization.cpp
@@ -4,9 +4,15 @@
 #include "TempHandlers.h"
 #include "HeaterControl.h"
 
+// Humidity Sensor 1
 #define I2C0_PORT i2c0
-#define I2C0_SDA  16
-#define I2C0_SCL  17
+#define I2C0_SDA 16
+#define I2C0_SCL 17
+
+// Humidity Sensor 2
+#define I2C1_PORT i2c1
+#define I2C1_SDA 14
+#define I2C1_SCL 15
 
 #define HEATER_PIN  20
 #define HEATER_FREQ 0.6f
@@ -28,25 +34,30 @@ int main()
     stdio_init_all();
     sleep_ms(3000);
 
-    i2c_setup(I2C0_PORT, I2C0_SDA, I2C0_SCL, 400 * 1000);
 
-    void *sensor = setup_sht4x(i2c0, 'A');
+    //Init I2C
+    i2c_setup(I2C0_PORT, I2C0_SDA, I2C0_SCL, 400 * 1000);
+    i2c_setup(I2C1_PORT, I2C1_SDA, I2C1_SCL, 400 * 1000);
+
+    void *SHT40_1 = setup_sht4x(i2c0, 'A'); // setup_sht4x generates a pointer handle for the sensor
+    void *SHT40_2 = setup_sht4x(i2c1, 'A');
+
+    SensorReading reading_inside = get_sensor_reading(SHT40_1);
+    SensorReading reading_outside = get_sensor_reading(SHT40_2);
 
     HeaterControl heater(HEATER_PIN, HEATER_FREQ);
     heater.init();
     heater.set_duty(HEATER_DUTY);
 
-    printf("time_ms, temp, humidity\n");
+    printf("time_ms, temp_inside, humidity_inside, error_inside, temp_outside, humidity_outside, error_outside\n");
 
     uint32_t start = to_ms_since_boot(get_absolute_time());
 
     while (true) {
-        SensorReading r = get_sensor_reading(sensor);
-
-        if (r.error == 0) {
-            uint32_t t = to_ms_since_boot(get_absolute_time()) - start;
-            printf("%lu, %.2f, %.2f\n", t, r.temp, r.humidity);
-        }
+        reading_inside = get_sensor_reading(SHT40_1);
+        reading_outside = get_sensor_reading(SHT40_2);
+        uint32_t t = to_ms_since_boot(get_absolute_time()) - start;
+        printf("%lu, %.3f, %.3f, %d, %.3f, %.3f, %d\n", t, reading_inside.temp, reading_inside.humidity, reading_inside.error, reading_outside.temp, reading_outside.humidity, reading_outside.error);
 
         sleep_ms(SAMPLE_MS);
     }
